3a5c6474836ddc328c66bf1b8c9b85f5189a6e21
cbethune <cbethune@uncharted.software>
2018-09-13 00:46:07 -0400

    split command line and executor apart

--- distil-pipeline-runner/pipeline_executor.py
+++ distil-pipeline-runner/pipeline_executor.py
@@ -2,7 +2,6 @@
 
 import time
 import os
-import sys
 from typing import List, Dict, Any
 from concurrent import futures
 
@@ -18,25 +17,25 @@ from d3m.metadata import base as metadata_base
 import frozendict
 
 
-input_table: List[container.Dataset] = []
-output_table: List[Dict[str, Any]] = []
+_input_table: List[container.Dataset] = []
+_output_table: List[Dict[str, Any]] = []
 
-def resolve_output(dataref: str) -> List[Dict[str, Any]]:
+def _resolve_output(dataref: str) -> List[Dict[str, Any]]:
     # on inputs use the input dataset, on subsequent
     dataref_parts = dataref.split(".")
     if dataref_parts[0] == 'inputs':
-        return input_table[int(dataref_parts[1])]
+        return _input_table[int(dataref_parts[1])]
     if dataref_parts[0] == 'steps':
-        return output_table[int(dataref_parts[1])][dataref_parts[2]]
+        return _output_table[int(dataref_parts[1])][dataref_parts[2]]
     return []
 
-def get_input(primitive_step: pipeline_pb2.PrimitivePipelineDescriptionStep) -> List[Dict[str, Any]]:
+def _get_input(primitive_step: pipeline_pb2.PrimitivePipelineDescriptionStep) -> List[Dict[str, Any]]:
     # get the input data reference
     inputs = primitive_step.arguments['inputs']
     dataref = inputs.container.data
-    return resolve_output(dataref)
+    return _resolve_output(dataref)
 
-def get_hyperparameters(primitive_step: pipeline_pb2.PrimitivePipelineDescriptionStep, primitive_class: frozendict.FrozenOrderedDict) -> Any:
+def _get_hyperparameters(primitive_step: pipeline_pb2.PrimitivePipelineDescriptionStep, primitive_class: frozendict.FrozenOrderedDict) -> Any:
     # parse out hyperparameter structures and initialize with defaults
     primitive_hyperparams_class = primitive_class.metadata.query()['primitive_code']['class_type_arguments']['Hyperparams']
     hyperparams = primitive_hyperparams_class.defaults()
@@ -45,38 +44,38 @@ def get_hyperparameters(primitive_step: pipeline_pb2.PrimitivePipelineDescriptio
     # iterate over entries and add values to hyperparameter struct
     for hyperparam_key, hyperparam_value in primitive_step.hyperparams.items():
         if hyperparam_value.WhichOneof('argument') == 'value':
-            parsed_param = get_raw_hyperparameter(hyperparam_value.value.data.raw)
+            parsed_param = _get_raw_hyperparameter(hyperparam_value.value.data.raw)
             new_hyperparams[hyperparam_key] = parsed_param
 
     return new_hyperparams
 
-def get_list_hyperparameter(list: value_pb2.ValueList) -> Any:
+def _get_list_hyperparameter(list: value_pb2.ValueList) -> Any:
     result_list = []
     for value in list.items:
         which_value = value.WhichOneof('raw')
         if which_value == 'list':
-            result_list.append(get_list_hyperparameter(value.list))
+            result_list.append(_get_list_hyperparameter(value.list))
         elif which_value == 'dict':
-            result_list.append(get_dict_hyperparameter(value.dict))
+            result_list.append(_get_dict_hyperparameter(value.dict))
         else:
-            result_list.append(get_raw_hyperparameter(value))
+            result_list.append(_get_raw_hyperparameter(value))
 
     return result_list
 
-def get_dict_hyperparameter(dict: value_pb2.ValueDict) -> Any:
+def _get_dict_hyperparameter(dict: value_pb2.ValueDict) -> Any:
     result_dict: Dict[str, Any] = {}
     for key, value in dict.items.items():
         which_value = value.WhichOneof('raw')
         if which_value == 'list':
-            result_dict[key] = get_list_hyperparameter(value.list)
+            result_dict[key] = _get_list_hyperparameter(value.list)
         elif which_value == 'dict':
-            result_dict[key] = get_dict_hyperparameter(value.dict)
+            result_dict[key] = _get_dict_hyperparameter(value.dict)
         else:
-            result_dict[key] = get_raw_hyperparameter(value)
+            result_dict[key] = _get_raw_hyperparameter(value)
 
     return result_dict
 
-def get_raw_hyperparameter(value: value_pb2.ValueRaw) -> Any:
+def _get_raw_hyperparameter(value: value_pb2.ValueRaw) -> Any:
     which_value = value.WhichOneof('raw')
     if which_value == 'null':
         return value.null
@@ -91,13 +90,13 @@ def get_raw_hyperparameter(value: value_pb2.ValueRaw) -> Any:
     if which_value == 'bytes':
         return value.bytes
     if which_value == 'list':
-        return get_list_hyperparameter(value.list)
+        return _get_list_hyperparameter(value.list)
     if which_value == 'dict':
-        return get_dict_hyperparameter(value.dict)
+        return _get_dict_hyperparameter(value.dict)
 
     return None
 
-def get_static_resources(primitive_class: frozendict.FrozenOrderedDict, static_res_path: str) -> Dict[str, str]:
+def _get_static_resources(primitive_class: frozendict.FrozenOrderedDict, static_res_path: str) -> Dict[str, str]:
     # create a table of static resource paths for this primitive if specified
     volumes = {}
     for installation in primitive_class.metadata.query()['installation']:
@@ -105,7 +104,33 @@ def get_static_resources(primitive_class: frozendict.FrozenOrderedDict, static_r
             volumes[installation['key']] = os.path.join(static_res_path, installation['key'])
     return volumes
 
-def execute_pipeline(pipeline: pipeline_pb2.PipelineDescription, static_res_path: str) -> Any:
+def _load_pipeline(filename: str) -> pipeline_pb2.PipelineDescription:
+    f = open(filename, "rb")
+    pipeline = pipeline_pb2.PipelineDescription()
+    pipeline.ParseFromString(f.read())
+    f.close()
+    return pipeline
+
+def execute_pipeline(pipeline_filename, dataset_filename, static_resource_path = None) -> Any:
+    """
+        Executes a binrary protobuf pipeline against a supplied d3m dataset.
+
+    Parameters
+    ----------
+    pipeline_filename: path to binary protobuf pipeline definition
+    dataset_filename: path to folder containing a D3M dataset
+
+    Returns
+    -------
+    The result of the pipeline execution.
+    """
+    # load the protobuf pipeline def
+    pipeline = _load_pipeline(pipeline_filename)
+
+    # load the input dataset
+    input_dataset = container.Dataset.load(dataset_filename)
+    _input_table.append(input_dataset)
+
     steps = pipeline.steps
     for step in steps:
         # load the primitive class
@@ -114,57 +139,21 @@ def execute_pipeline(pipeline: pipeline_pb2.PipelineDescription, static_res_path
         primitive_class = getattr(module, name)
 
         # get the static resource table, hyperparams, and instantiate the primitive
-        static_resources = get_static_resources(primitive_class, static_res_path)
-        hyperparams = get_hyperparameters(step.primitive, primitive_class)
+        static_resources = _get_static_resources(primitive_class, static_resource_path)
+        hyperparams = _get_hyperparameters(step.primitive, primitive_class)
 
         primitive = None
-        if static_res_path:
+        if static_resource_path:
             primitive = primitive_class(hyperparams=hyperparams, volumes=static_resources)
         else:
             primitive = primitive_class(hyperparams=hyperparams)
 
         # reflectively call each output method using the hyperparams
         for output in step.primitive.outputs:
-            input_data = get_input(step.primitive)
-
+            input_data = _get_input(step.primitive)
             result = getattr(primitive, output.id)(inputs=input_data).value
-
-            output_table.append({output.id: result})
+            _output_table.append({output.id: result})
 
     # extract the final output
     output_dataref = pipeline.outputs[0].data
-    return resolve_output(output_dataref)
-
-def load_pipeline(filename: str) -> pipeline_pb2.PipelineDescription:
-    f = open(filename, "rb")
-    pipeline = pipeline_pb2.PipelineDescription()
-    pipeline.ParseFromString(f.read())
-    f.close()
-    return pipeline
-
-def main():
-    # path to pipeline file
-    pipeline_filename = sys.argv[1]
-
-    # path to dataset
-    dataset_filename = sys.argv[2]
-
-    # optional resource path
-    static_resource_path = None
-    if len(sys.argv) >= 4:
-        static_resource_path = sys.argv[3]
-
-    # load the protobuf pipeline def
-    pipeline = load_pipeline(pipeline_filename)
-
-    # load the input dataset
-    input_dataset = container.Dataset.load(dataset_filename)
-    input_table.append(input_dataset)
-
-    # execute the pipeline
-    output = execute_pipeline(pipeline, static_resource_path)
-
-    print(output)
-
-if __name__ == "__main__":
-    main()
+    return _resolve_output(output_dataref)
---
+++ distil-pipeline-runner/run_pipeline.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+import sys
+import pipeline_executor as pe
+
+if __name__ == "__main__":
+   # path to pipeline file
+    pipeline_filename = sys.argv[1]
+
+    # path to dataset
+    dataset_filename = sys.argv[2]
+
+    # optional resource path
+    static_resource_path = None
+    if len(sys.argv) >= 4:
+        static_resource_path = sys.argv[3]
+
+    # execute the pipeline against the data
+    output = pe.execute_pipeline(pipeline_filename, dataset_filename, static_resource_path)
+    print(output)
