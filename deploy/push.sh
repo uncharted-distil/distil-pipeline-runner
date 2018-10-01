2d2b6dfa38807833097e725c5dc34d5cedd21b00
kbirk <birk.kevin@gmail.com>
2018-10-01 16:45:06 -0400

    Update push.sh.

--- deploy/push.sh
+++ deploy/push.sh
@@ -2,5 +2,9 @@
 
 source ./config.sh
 
+docker login docker.uncharted.software
+
 docker push docker.uncharted.software/$DOCKER_IMAGE_NAME:${DOCKER_IMAGE_VERSION}
 docker push docker.uncharted.software/$DOCKER_IMAGE_NAME:latest
+
+docker logout docker.uncharted.software
