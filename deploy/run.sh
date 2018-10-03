c00cbbc5ada6a1cb833573d025e7754dd05a647d
Philippe Horne <phorne@uncharted.software>
2018-10-03 07:23:14 -0400

    Added params to the container when running.

--- deploy/run.sh
+++ deploy/run.sh
@@ -4,7 +4,9 @@ source ./config.sh
 
 docker run \
     -p 50051:50051 \
-    -v /usr/local/d3m/datasets:/usr/local/d3m/datasets \
-    -v /usr/local/d3m/output:/usr/local/d3m/output \
-    -v /usr/local/d3m/static_resources:/usr/local/d3m/static_resources \
+    --env D3MOUTPUTDIR=$D3MOUTPUTDIR \
+    --env STATIC_RESOURCE_PATH=$STATIC_RESOURCE_PATH \
+    -v /input:/input \
+    -v $D3MOUTPUTDIR:$D3MOUTPUTDIR \
+    -v $STATIC_RESOURCE_PATH:$STATIC_RESOURCE_PATH \
      docker.uncharted.software/$DOCKER_IMAGE_NAME:latest
