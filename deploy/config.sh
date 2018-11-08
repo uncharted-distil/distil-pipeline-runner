42051216616927557a593db79754f8dbeff2fb72
cbethune <cbethune@uncharted.software>
2018-11-08 10:01:20 -0500

    update to key name to better denote purpose

--- deploy/config.sh
+++ deploy/config.sh
@@ -8,11 +8,11 @@ DOCKER_IMAGE_VERSION=0.1.0
 D3MINPUTDIR=~/data/d3m_data/datasets/seed_datasets_current
 
 # temperary output location
-D3MOUTPUTDIR=/home/chris/dev/go_workspace/src/github.com/unchartedsoftware/distil/outputs
+D3MOUTPUTDIR=~/dev/go_workspace/src/github.com/unchartedsoftware/distil/outputs
 
 # D3M static models directory
-STATIC_RESOURCE_PATH=/home/chris/data/d3m_models
+STATIC_RESOURCE_PATH=~/data/d3m_models
 
 # Private GitHub repo key locations
-SSH_KEY_LOC=~/keys/d3mbuild/distil-primitives/distil-primitives_rsa
+SSH_KEY_LOC=~/keys/d3mbuild/distil-pipeline-runner-build/distil-pipeline-runner-build_rsa
 
