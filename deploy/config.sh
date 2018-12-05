91cb04d947d648f3eecb954aa20a5c127ef5085f
kbirk <birk.kevin@gmail.com>
2018-12-05 16:39:20 -0500

    Update config.sh

--- deploy/config.sh
+++ deploy/config.sh
@@ -5,14 +5,13 @@ DOCKER_IMAGE_NAME=distil-pipeline-runner
 DOCKER_IMAGE_VERSION=0.1.1
 
 # base dataset location
-D3MINPUTDIR=~/data/d3m_data/datasets/seed_datasets_current
+D3MINPUTDIR=/data/datasets/seed_datasets_current
 
 # temperary output location
-D3MOUTPUTDIR=~/dev/go_workspace/src/github.com/unchartedsoftware/distil/outputs
+D3MOUTPUTDIR=/data/output
 
 # D3M static models directory
-STATIC_RESOURCE_PATH=~/data/d3m_models
+STATIC_RESOURCE_PATH=/data/static_resources
 
 # Private GitHub repo key locations
 SSH_KEY_LOC=~/keys/d3mbuild/distil-pipeline-runner-build/distil-pipeline-runner-build_rsa
-
