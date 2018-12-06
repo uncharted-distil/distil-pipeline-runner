e2b5c2f38d8ea73fbea579e4a11775b5f080f3a5
kbirk <birk.kevin@gmail.com>
2018-12-06 11:45:15 -0500

    Split docker file up.

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,6 +4,17 @@ source ./config.sh
 
 # builds distil docker image
 docker build \
+    -f ../Dockerfile.env \
+    --tag docker.uncharted.software/$DOCKER_IMAGE_NAME-env:${DOCKER_IMAGE_VERSION} \
+    --tag docker.uncharted.software/$DOCKER_IMAGE_NAME-env:latest ..
+
+docker build \
+    -f ../Dockerfile.d3m \
+    --tag docker.uncharted.software/$DOCKER_IMAGE_NAME-d3m:${DOCKER_IMAGE_VERSION} \
+    --tag docker.uncharted.software/$DOCKER_IMAGE_NAME-d3m:latest ..
+
+docker build \
+    -f ../Dockerfile \
     --squash \
     --build-arg SSH_KEY="$(cat $SSH_KEY_LOC)" \
     --tag docker.uncharted.software/$DOCKER_IMAGE_NAME:${DOCKER_IMAGE_VERSION} \
