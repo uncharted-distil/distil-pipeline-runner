fb5bcd68a645caf95cd340727914055bc37851c2
cbethune <cbethune@uncharted.software>
2019-01-02 15:58:49 -0500

    fixes join test run script

--- test/run_join.sh
+++ test/run_join.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python3 ../pipelinerunner/run_pipeline.py \
-    ./join_test.pln \
+    ./join.pln \
     file://$D3MINPUTDIR/185_baseball/TRAIN/dataset_TRAIN/datasetDoc.json \
     file://$D3MINPUTDIR/185_baseball/TRAIN/dataset_TRAIN/datasetDoc.json \
     -r $STATIC_RESOURCE_PATH -v
