46a2ecd588107479a9a3bd545513daa943abcb23
cbethune <cbethune@uncharted.software>
2019-02-12 13:12:22 -0500

    updates NK primitives to feb release

--- deploy/config.sh
+++ deploy/config.sh
@@ -2,7 +2,7 @@
 
 # name and version of docker image that will be created
 DOCKER_IMAGE_NAME=distil-pipeline-runner
-DOCKER_IMAGE_VERSION=0.1.12
+DOCKER_IMAGE_VERSION=0.1.13
 
 # base dataset location
 D3MINPUTDIR=/data/datasets/seed_datasets_current
