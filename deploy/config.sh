10d8e638b3667ba4758a58976d6ba1dc506372d5
cbethune <cbethune@uncharted.software>
2019-05-18 12:06:33 -0400

    version bump

--- deploy/config.sh
+++ deploy/config.sh
@@ -2,7 +2,7 @@
 
 # name and version of docker image that will be created
 DOCKER_IMAGE_NAME=distil-pipeline-runner
-DOCKER_IMAGE_VERSION=0.1.66
+DOCKER_IMAGE_VERSION=0.2.0
 
 # base dataset location
 D3MINPUTDIR=/data/datasets/seed_datasets_current
